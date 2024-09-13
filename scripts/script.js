fec282a8c96d769b56c84fd610c4627a68affb93
LarsVeldhuijs <118227652+LarsVeldhuijs@users.noreply.github.com>
2024-09-13 10:46:18 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,11 @@
 // JavaScript Document
 console.log("hi");
+
+var menuButton = document.querySelector("button:first-of-type");
+var navUl = document.querySelector("nav");
+
+menuButton.addEventListener("click", openMenu);
+
+function openMenu(){
+    navUl.classList.toggle("toonMenu");
+}
